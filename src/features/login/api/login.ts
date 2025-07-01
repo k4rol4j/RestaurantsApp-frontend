7359db2ba3c7b67c3fa5f5c3faf1ab5387ad199f
k4rol4j <jedryczka@student.agh.edu.pl>
2025-07-01 08:55:09 +0200

    Poprawki

--- src/features/login/api/login.ts
+++ src/features/login/api/login.ts
@@ -1,17 +1,14 @@
-import { API_URL } from "../../../config.ts";
+import {API_URL} from "../../../config.ts";
 
 export const login = async (username: string, password: string) => {
     const response = await fetch(`${API_URL}/auth/login`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            Authorization: `Basic ${window.btoa(username + ":" + password)}`,
         },
         credentials: 'include',
-        body: JSON.stringify({
-            email: username,   // lub login: username jeśli backend tak oczekuje
-            password: password,
-        }),
     });
-    if (!response.ok) throw new Error('Login failed.');
+    if (response.status !== 200) throw new Error('Login failed.');
     return await response.text();
-};
+}
